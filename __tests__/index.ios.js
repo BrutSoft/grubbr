48713b63341d353ecfc9a90313539a6a25573ab4
Nathan Donolli <ndonolli@gmail.com>
2016-10-21 08:40:37 -0500

    add jest config for render testing

--- __tests__/index.ios.js
+++ __tests__/index.ios.js
@@ -1,12 +1,20 @@
 import 'react-native';
 import React from 'react';
+<<<<<<< cd3fc301b29ab312d0317c7aa98c7a037e29d0e8
 import Grubbr from '../index.ios.js';
+=======
+import grubbr from '../index.ios.js';
+>>>>>>> add jest config for render testing
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
 it('renders correctly', () => {
   const tree = renderer.create(
+<<<<<<< cd3fc301b29ab312d0317c7aa98c7a037e29d0e8
     <Grubbr />
+=======
+    <grubbr />
+>>>>>>> add jest config for render testing
   );
 });
