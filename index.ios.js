8df172057ce6375cfd5d617703029dfd262a9e84
Nathan Donolli <ndonolli@gmail.com>
2016-10-21 16:54:50 -0500

    refactor entry point for android

--- index.ios.js
+++ index.ios.js
@@ -7,21 +7,8 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-//import Reducers from './app/reducers';
 import AppNavigator from './app/AppNavigator';
 import { AppRegistry } from 'react-native';
 import setup from './app/setup';
-
-//const store = createStore(Reducers);
-
-export default class Grubbr extends Component {
-  render() {
-    return (
-      <Provider>
-        <AppNavigator />
-      </Provider>
-    );
-  }
-}
 
 AppRegistry.registerComponent('grubbr', setup);
