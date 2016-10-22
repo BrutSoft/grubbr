8ee33c6a0ed2ea6b5f21ca2daea330830b5a22bd
Nathan Donolli <ndonolli@gmail.com>
2016-10-21 19:24:58 -0500

    add console logs for debugging

--- app/chooseFood.js
+++ app/chooseFood.js
@@ -1,29 +1,92 @@
 import React, { Component } from 'react';
-import { Container, Content, Button } from 'native-base';
+import { connect } from 'react-redux';
+import { Container, Header, Title, Content, Text, Button, Icon } from 'native-base';
+import { openDrawer, closeDrawer } from './actions/drawer';
+import { replaceRoute, popRoute , pushNewRoute } from './actions/route';
+import { setIndex } from './actions/list';
 
-function Choices(props) {
-  return (
-      <Content>
-        <Button block rounded
-          onPress={() => {
-            props.dispatch({ id: 'knowWhat'});
-          }}>
-          I know WHAT I want
-        </Button>
-        <Button block rounded
-          onPress={() => {
-            props.dispatch({ id: 'knowWhere' });
-          }}>
-          I know WHERE I go
-        </Button>
-        <Button block rounded
-          onPress={() => {
-            props.dispatch({ id: 'noClue' });
-          }}>
-          I ain't got a clue
-        </Button>
-      </Content>
-  )
+class Choices extends Component {
+
+  static propTypes = {
+    openDrawer: React.PropTypes.func,
+    closeDrawer: React.PropTypes.func,
+    replaceRoute: React.PropTypes.func,
+    replaceOrPushRoute: React.PropTypes.func,
+    pushNewRoute: React.PropTypes.func,
+    popRoute: React.PropTypes.func,
+    setIndex: React.PropTypes.func,
+    name: React.PropTypes.string,
+    list: React.PropTypes.arrayOf(React.PropTypes.string),
+  }
+
+  replaceRoute(route) {
+    this.props.replaceRoute(route);
+  }
+
+  pushNewRoute(route, index) {
+    this.props.setIndex(index);
+    this.props.pushNewRoute(route);
+  }
+
+  popRoute() {
+    console.log(this)
+  }
+
+  render() {
+    return (
+      <Container>
+        <Header>
+          <Button transparent onPress={() => this.popRoute()}>
+            <Icon name="ios-arrow-back" />
+          </Button>
+
+          <Title>Grubbr</Title>
+
+          <Button transparent onPress={this.props.openDrawer}>
+            <Icon name="ios-menu" />
+          </Button>
+        </Header>
+
+        <Content>
+          <Button block rounded
+            onPress={() => {
+              this.popRoute()
+            }}>
+            I know WHAT I want
+          </Button>
+          <Button block rounded
+            onPress={() => {
+              props.dispatch({ id: 'knowWhere' });
+            }}>
+            I know WHERE I go
+          </Button>
+          <Button block rounded
+            onPress={() => {
+              props.dispatch({ id: 'noClue' });
+            }}>
+            I ain't got a clue
+          </Button>
+        </Content>
+
+      </Container>
+    );
+  }
 }
 
-export default Choices;
+function bindAction(dispatch) {
+  return {
+    openDrawer: () => dispatch(openDrawer()),
+    replaceRoute: route => dispatch(replaceRoute(route)),
+    pushNewRoute: route => dispatch(pushNewRoute(route)),
+    setIndex: index => dispatch(setIndex(index)),
+  };
+}
+
+function mapStateToProps(state) {
+  return {
+    name: state.user.name,
+    list: state.list.list,
+  };
+}
+
+export default connect(null, bindAction)(Choices);
--- app/main.js
+++ app/main.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
+import { Navigator } from 'react-native';
 import { connect } from 'react-redux';
 import { Container, Header, Title, Content, Text, Button, Icon } from 'native-base';
 import { openDrawer, closeDrawer } from './actions/drawer';
-import { replaceRoute, replaceOrPushRoute, pushNewRoute } from './actions/route';
+import { replaceRoute, popRoute , pushNewRoute } from './actions/route';
 import { setIndex } from './actions/list';
 
 class Main extends Component {
@@ -13,6 +14,7 @@ class Main extends Component {
     replaceRoute: React.PropTypes.func,
     replaceOrPushRoute: React.PropTypes.func,
     pushNewRoute: React.PropTypes.func,
+    popRoute: React.PropTypes.func,
     setIndex: React.PropTypes.func,
     name: React.PropTypes.string,
     list: React.PropTypes.arrayOf(React.PropTypes.string),
@@ -27,12 +29,31 @@ class Main extends Component {
     this.props.pushNewRoute(route);
   }
 
+  popRoute() {
+    this.props.popRoute();
+  }
+
+  renderScene(route, navigator) { // eslint-disable-line class-methods-use-this
+    switch (route.id) {
+      case 'splashscreen':
+        return <SplashPage navigator={navigator} />;
+      case 'login':
+        return <Login navigator={navigator} />;
+      case 'main':
+        return <Main navigator={navigator} />;
+      case 'chooseFood':
+        return <Choices navigator={navigator} />;
+      default :
+        return <Login navigator={navigator} />;
+    }
+  }
+
   render() {
     return (
       <Container>
         <Header>
-          <Button transparent onPress={() => this.replaceRoute('login')}>
-            <Icon name="ios-power" />
+          <Button transparent onPress={() => this.popRoute()}>
+            <Icon name="ios-arrow-back" />
           </Button>
 
           <Title>Grubbr</Title>
@@ -45,7 +66,8 @@ class Main extends Component {
         <Content>
           <Button block rounded
             onPress={() => {
-              this.pushNewRoute('findGrub')
+              console.log(this);
+              this.pushNewRoute('chooseFood')
             }}>
             Find Grub
           </Button>
