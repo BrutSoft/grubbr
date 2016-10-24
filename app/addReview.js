c152d877ea88bab301e24906347321c0adb9708c
Nathan Donolli <ndonolli@gmail.com>
2016-10-24 10:12:42 -0500

    refactor addreview

--- app/addReview.js
+++ app/addReview.js
@@ -1,9 +1,27 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { Image } from 'react-native';
 import { Container, Content, DeckSwiper, Title, Header, InputGroup, Input, Icon, Button, View, Card, CardItem, Thumbnail, Text, List, ListItem, Picker } from 'native-base';
 
+import { openDrawer, closeDrawer } from './actions/drawer';
+import { replaceRoute, popRoute , pushNewRoute } from './actions/route';
+import { setIndex } from './actions/list';
+
 const Item = Picker.Item;
 class AddReview extends Component {
+
+  static propTypes = {
+    openDrawer: React.PropTypes.func,
+    closeDrawer: React.PropTypes.func,
+    replaceRoute: React.PropTypes.func,
+    replaceOrPushRoute: React.PropTypes.func,
+    pushNewRoute: React.PropTypes.func,
+    popRoute: React.PropTypes.func,
+    setIndex: React.PropTypes.func,
+    name: React.PropTypes.string,
+    list: React.PropTypes.arrayOf(React.PropTypes.string),
+  }
+
   constructor(props) {
     super(props);
       this.state = {
@@ -14,6 +32,20 @@ class AddReview extends Component {
           }
       }
   }
+
+  replaceRoute(route) {
+    this.props.replaceRoute(route);
+  }
+
+  pushNewRoute(route, index) {
+    this.props.setIndex(index);
+    this.props.pushNewRoute(route);
+  }
+
+  popRoute() {
+    this.props.popRoute();
+  }
+
   onValueChange (value: string) {
     this.setState({
       selected1 : value
@@ -23,6 +55,18 @@ class AddReview extends Component {
   render () {
     return (
       <Container>
+        <Header>
+          <Button transparent onPress={() => this.popRoute()}>
+            <Icon name="ios-arrow-back" />
+          </Button>
+
+          <Title>Grubbr</Title>
+
+          <Button transparent onPress={this.props.openDrawer}>
+            <Icon name="ios-menu" />
+          </Button>
+        </Header>
+
         <Content>
           <Title>
             <Text>Add Review</Text>
@@ -77,4 +121,21 @@ class AddReview extends Component {
   }
 }
 
-export default AddReview;
+function bindAction(dispatch) {
+  return {
+    openDrawer: () => dispatch(openDrawer()),
+    replaceRoute: route => dispatch(replaceRoute(route)),
+    pushNewRoute: route => dispatch(pushNewRoute(route)),
+    setIndex: index => dispatch(setIndex(index)),
+    popRoute: () => dispatch(popRoute())
+  };
+}
+
+function mapStateToProps(state) {
+  return {
+    name: state.user.name,
+    list: state.list.list,
+  };
+}
+
+export default connect(mapStateToProps, bindAction)(AddReview);
