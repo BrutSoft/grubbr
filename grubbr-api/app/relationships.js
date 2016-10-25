c40836cf5cfc19a6703ea50af1fcac5e3642a7bc
David d'Aquin <david@daquin.com>
2016-10-25 14:43:00 -0500

    added relationship

--- grubbr-api/app/relationships.js
+++ grubbr-api/app/relationships.js
@@ -1,5 +1,3 @@
-'use strict';
-
 const Nodal = require('nodal');
 
 /* Include this file to enable Model relationships */
@@ -13,22 +11,26 @@ const Nodal = require('nodal');
 
 */
 
-const Adjective =  Nodal.require('app/models/adjective.js');
+const Adjective = Nodal.require('app/models/adjective.js');
 const Dish = Nodal.require('app/models/dish.js');
 const DishType = Nodal.require('app/models/dish_type.js');
 const Favorite = Nodal.require('app/models/favorite.js');
 const MenuType = Nodal.require('app/models/menu_type.js');
 const Rating = Nodal.require('app/models/rating.js');
 const User = Nodal.require('app/models/user.js');
-const Restaurant = Nodal.require('app/models/restaurant.js')
+const Restaurant = Nodal.require('app/models/restaurant.js');
+const DishDishType = Nodal.require('app/models/dish_dish_type.js');
 
-Rating.joinsTo(Adjective, {multiple: true});
-Rating.joinsTo(Dish, {multiple: true});
-Rating.joinsTo(User, {multiple: true});
-Dish.joinsTo(Restaurant, {multiple: true});
-Dish.joinsTo(MenuType, {multiple: true});
-Dish.joinsTo(DishType, {multiple: true});
-Favorite.joinsTo(User, {multiple: true});
-Favorite.joinsTo(Dish, {multiple: true});
+Rating.joinsTo(Adjective, { multiple: true });
+Rating.joinsTo(Dish, { multiple: true });
+Rating.joinsTo(User, { multiple: true });
+Dish.joinsTo(Restaurant, { multiple: true });
+Dish.joinsTo(MenuType, { multiple: true });
+// Dish.joinsTo(DishType, { multiple: true });
+Favorite.joinsTo(User, { multiple: true });
+Favorite.joinsTo(Dish, { multiple: true });
+DishDishType.joinsTo(Dish, { multiple: true });
+DishDishType.joinsTo(DishType, { multiple: true });
+
 
 module.exports = {}; // Don't need to export anything
