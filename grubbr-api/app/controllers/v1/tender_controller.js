62cef68afba090164b62f8fdbc3c0ff5c7057b40
David d'Aquin <david@daquin.com>
2016-10-26 18:23:51 -0500

    tender fully functional

--- grubbr-api/app/controllers/v1/tender_controller.js
+++ grubbr-api/app/controllers/v1/tender_controller.js
@@ -1,23 +1,53 @@
 const Nodal = require('nodal');
+const _ = require('underscore');
 
-const Dishes = Nodal.require('app/models/dish.js');
 const Ratings = Nodal.require('app/models/rating.js');
 
 class V1TenderController extends Nodal.Controller {
 
   get() {
-    Dishes.query()
-      .where(this.params.query)
+    Ratings.query()
+      .join('dish__restaurant')
+      .join('dish__menuType')
       .end((err, models) => {
-        if (err) {
-          this.respond(err, models);
-        }
-        const ids = models.map(model => model.get('id'));
-        Ratings.query()
-          .where({}, { dish_id: ids })
-          .end((err2, ratings) => {
-            this.respond(err2 || ratings);
-          });
+        let dishInfo = {};
+        // consolidate all info from ratings and dishes into one object
+        models.forEach((rating) => {
+          // get the dish id this will be compiled under
+          const dishID = rating.joined('dish').get('id');
+          // check if there is already a property for that dish
+          if (dishInfo[dishID] === undefined) {
+            // create the one time data for it now.
+            dishInfo[dishID] = {};
+            dishInfo[dishID].dishID = dishID;
+            dishInfo[dishID].dishName = rating.joined('dish').get('name');
+            dishInfo[dishID].upvotes = 0;
+            dishInfo[dishID].downvotes = 0;
+            dishInfo[dishID].score = 0;
+            dishInfo[dishID].images = [];
+            dishInfo[dishID].restaurantName = rating.joined('dish').joined('restaurant').get('name');
+            dishInfo[dishID].restaurantID = rating.joined('dish').joined('restaurant').get('id');
+          }
+          // do all the things that are normally done now.
+          if (rating.get('rating') === 1) {
+            dishInfo[dishID].upvotes += 1;
+            dishInfo[dishID].score += 1;
+          } else if (rating.get('rating') === -1) {
+            dishInfo[dishID].downvotes -= 1;
+            dishInfo[dishID].score -= 1;
+          }
+          if (rating.get('image')) { dishInfo[dishID].images.push(rating.get('image')); }
+        });
+        // Put that info into an array
+        dishInfo = _.map(dishInfo, info => info);
+        // sort info by score
+        dishInfo = dishInfo.sort((a, b) => {
+          if (a.score > b.score) { return -1; }
+          if (a.score < b.score) { return 1; }
+          return 0;
+        });
+        // send that array back in response.
+        this.respond(dishInfo);
       });
   }
 }
