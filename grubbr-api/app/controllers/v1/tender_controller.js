3e7fce0d151f1677a9fc2b37b4fedae1a8493c89
David d'Aquin <david@daquin.com>
2016-10-26 16:10:40 -0500

    first draft of dishes query into ratings, will redo now

--- grubbr-api/app/controllers/v1/tender_controller.js
+++ grubbr-api/app/controllers/v1/tender_controller.js
@@ -1,16 +1,25 @@
 const Nodal = require('nodal');
 
-const Dishes = Nodal.require('app/models/dish.js')
+const Dishes = Nodal.require('app/models/dish.js');
 const Ratings = Nodal.require('app/models/rating.js');
 
 class V1TenderController extends Nodal.Controller {
 
   get() {
-    // get all dishes, compile needed info from other
-    // this.respond({ message: `GET request to ${this.constructor.name}`, this: this });
-
+    Dishes.query()
+      .where(this.params.query)
+      .end((err, models) => {
+        if (err) {
+          this.respond(err, models);
+        }
+        const ids = models.map(model => model.get('id'));
+        Ratings.query()
+          .where({}, { dish_id: ids })
+          .end((err2, ratings) => {
+            this.respond(err2 || ratings);
+          });
+      });
   }
-
 }
 
 module.exports = V1TenderController;
