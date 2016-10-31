a89d118809ff6311077d53cf27144c086b914430
Zivile <zivilytez@hotmail.com>
2016-10-31 14:42:57 -0500

    Revert "npm installed rn-cameral-roll"
    This reverts commit 627dbed21ac79de51322b7bd7d842de6ea0731d9.

--- android/app/src/main/java/com/grubbr/MainApplication.java
+++ android/app/src/main/java/com/grubbr/MainApplication.java
@@ -4,7 +4,6 @@ import android.app.Application;
 import android.util.Log;
 
 import com.facebook.react.ReactApplication;
-import fr.bamlab.rncameraroll.CameraRollPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
@@ -26,7 +25,6 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new CameraRollPackage(),
             new VectorIconsPackage()
       );
     }
