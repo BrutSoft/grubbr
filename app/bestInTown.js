a14c4d7e79a34ba860b3389221bd62add961b4cb
Nathan Donolli <ndonolli@gmail.com>
2016-11-01 17:06:34 -0500

    add geolocation params to fetch uri in search and add catch logic

--- app/bestInTown.js
+++ app/bestInTown.js
@@ -17,18 +17,21 @@ class BestInTown extends Component {
     setIndex: React.PropTypes.func,
     setCurrentDish: React.PropTypes.func,
     setLocation: React.PropTypes.func,
-
+    location: React.PropTypes.object,
   }
+
   constructor(props) {
     super(props);
     this.state = {
-      search: null,
+      query: null,
+      error: false,
+      noResults: false,
+      searchedYet: false,
     };
   }
 
   componentDidMount() {
     this.getLocation();
-    this.search().done();
   }
 
   getLocation() {
@@ -36,7 +39,12 @@ class BestInTown extends Component {
       (position) => {
         this.setLocation(position);
       },
-      error => alert(JSON.stringify(error)),
+      () => {
+        this.setState({
+          error: true,
+          errorType: 'geo',
+        });
+      },
       { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
     );
   }
@@ -63,28 +71,49 @@ class BestInTown extends Component {
   }
 
   search() {
-      // Set loading to true when the search starts to display a Spinner
+    // Set loading to true when the search starts to display a Spinner
     this.setState({
       loading: true,
+      error: false,
+      searchedYet: true,
     });
     const that = this;
-    return fetch(`https://grubbr-api.herokuapp.com/v1/search?dish__name__icontains=${this.state.search}`)
+    const latitude = this.props.location.coords.latitude;
+    const longitude = this.props.location.coords.longitude;
+    const url = `https://grubbr-api.herokuapp.com/v1/search?latitude=${latitude}&longitude=${longitude}&dish__name__icontains=${this.state.query}`;
+    return fetch(url)
     .then(response => response.json())
     .then((responseJson) => {
-      that.setState({
-        dishes: responseJson.data,
-        loading: false,
-      });
+      if (responseJson.data.length === 0) {
+        that.setState({
+          error: true,
+          errorType: 'noResults',
+        });
+      } else {
+        that.setState({
+          dishes: responseJson.data,
+          loading: false,
+        });
+      }
     })
     .catch(() => {
       that.setState({
         loading: false,
-        noResults: true,
+        error: true,
+        errorType: 'fetch'
       });
     });
   }
 
-  render() {
+  renderError() {
+    let errorMessage;
+    if (this.state.errorType === 'geo') {
+      errorMessage = 'Uh oh! There was a problem sending your location to the server!';
+    } else if (this.state.errorType === 'fetch') {
+      errorMessage = 'There was a problem connecting to our delicious servers! Please try again in a short while';
+    } else if (this.state.errorType === 'noResults') {
+      errorMessage = 'No results Found! :('
+    }
     return (
       <Container style={styles.bgColor}>
         <Header>
@@ -98,15 +127,82 @@ class BestInTown extends Component {
         </Header>
 
         <Content style={styles.padding}>
-          <Title style={styles.title}>Best In Town</Title>
+          <Title style={styles.title}>Search Grub</Title>
           <InputGroup
             style={styles.search}
           >
             <Icon name="ios-search" />
             <Input
               placeholder="Search"
-              value={this.state.search}
-              onChangeText={text => this.setState({ search: text })}
+              value={this.state.query}
+              onChangeText={text => this.setState({ query: text })}
+              onSubmitEditing={() => this.search()}
+            />
+          </InputGroup>
+          <View style={styles.padding}>
+            <View>
+              <Text style={styles.errorMessage}>{errorMessage}</Text>
+            </View>
+          </View>
+        </Content>
+      </Container>
+    );
+  }
+
+  renderResults() {
+    if (!this.state.searchedYet) {
+      return false;
+    }
+    return (
+      <Card
+        style={styles.card}
+        dataArray={this.state.dishes}
+        renderRow={dish => (
+          <CardItem
+            button
+            onPress={() => {
+              this.setCurrentDish(dish);
+              this.pushNewRoute('foodProfile');
+            }}
+          >
+            <Thumbnail size={80} source={{ uri: dish.images[0] }} />
+            <Text>{dish.dishName}</Text>
+            <Icon name="ios-thumbs-up" />
+            <Text>{dish.upvotes}</Text>
+            <Icon name="ios-thumbs-down" />
+            <Text>{dish.downvotes}</Text>
+          </CardItem>
+          )}
+      />
+    );
+  }
+
+  render() {
+    if (this.state.error) {
+      return this.renderError();
+    }
+    return (
+      <Container style={styles.bgColor}>
+        <Header>
+          <Button transparent onPress={() => this.popRoute()}>
+            <Icon name="ios-arrow-back" />
+          </Button>
+          <Title>Grubbr</Title>
+          <Button transparent onPress={this.props.openDrawer}>
+            <Icon name="ios-menu" />
+          </Button>
+        </Header>
+
+        <Content style={styles.padding}>
+          <Title style={styles.title}>Find Grub</Title>
+          <InputGroup
+            style={styles.search}
+          >
+            <Icon name="ios-search" />
+            <Input
+              placeholder="Search"
+              value={this.state.query}
+              onChangeText={text => this.setState({ query: text })}
               onSubmitEditing={() => this.search()}
             />
           </InputGroup>
@@ -115,26 +211,7 @@ class BestInTown extends Component {
               <View>
                 <Spinner color="green" />
               </View> :
-                <Card
-                  style={styles.card}
-                  dataArray={this.state.dishes}
-                  renderRow={dish => (
-                    <CardItem
-                      button
-                      onPress={() => {
-                        this.setCurrentDish(dish);
-                        this.pushNewRoute('foodProfile');
-                      }}
-                    >
-                      <Thumbnail size={80} source={{ uri: dish.images[0] }} />
-                      <Text>{dish.dishName}</Text>
-                      <Icon name="ios-thumbs-up" />
-                      <Text>{dish.upvotes}</Text>
-                      <Icon name="ios-thumbs-down" />
-                      <Text>{dish.downvotes}</Text>
-                    </CardItem>
-                    )}
-                />
+                this.renderResults()
               }
           </View>
         </Content>
@@ -142,6 +219,7 @@ class BestInTown extends Component {
     );
   }
 }
+
 function bindAction(dispatch) {
   return {
     openDrawer: () => dispatch(openDrawer()),
