a34153bcff6b1f808b8a3cd9c06919837cf87706
David d'Aquin <david@daquin.com>
2016-11-02 17:27:59 -0500

    formatting and clean up

--- grubbr-api/app/controllers/v1/places_controller.js
+++ grubbr-api/app/controllers/v1/places_controller.js
@@ -7,28 +7,16 @@ class V1PlacesController extends Nodal.Controller {
 
   get() {
     const thisLat = this.params.query.latitude;
-    console.log(thisLat);
     const thisLong = this.params.query.longitude;
-    console.log(thisLong);
-    // this.respond({ message: `GET request to ${this.constructor.name}` });
-    // googleMapsClient.geocode({
-    //   address: '1600 Amphitheatre Parkway, Mountain View, CA',
-    // }, (err, response) => {
-    //   if (!err) {
-    //     this.respond(response);
-    //   }
-    // });
     googleMapsClient.placesNearby({
       language: 'en',
       location: [thisLat, thisLong],
       // radius: 16000,
       rankby: 'distance',
-      // minprice: 1,
-      // maxprice: 4,
       // opennow: true,
       type: 'restaurant',
     }, (err, results) => {
-      this.respond(results);
+      this.respond(results.json.results.map(model => model.name));
     });
   }
 
