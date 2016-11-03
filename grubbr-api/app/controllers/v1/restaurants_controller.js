84a997a6abf54aef4fabf95100f68e41ed9cb4dc
David d'Aquin <david@daquin.com>
2016-11-03 16:26:40 -0500

    now returns all google places info.

--- grubbr-api/app/controllers/v1/restaurants_controller.js
+++ grubbr-api/app/controllers/v1/restaurants_controller.js
@@ -1,13 +1,33 @@
-'use strict';
-
 const Nodal = require('nodal');
+const Promise = require('bluebird');
+const googleMapsClient = require('@google/maps').createClient({
+  key: process.env.GOOGLEPLACES_API,
+});
+
+const placesSearch = Promise.promisify(googleMapsClient.placesNearby);
 
 class V1RestaurantsController extends Nodal.Controller {
 
   get() {
+    const thisLat = this.params.query.latitude;
+    const thisLong = this.params.query.longitude;
 
-    this.respond({message: `GET request to ${this.constructor.name}`});
+    const searchOptions = {
+      language: 'en',
+      // radius: 16000,
+      rankby: 'distance',
+      // opennow: true,
+      type: 'restaurant',
+    }
 
+    if (thisLat && thisLong) { searchOptions.location = [thisLat, thisLong]; }
+    if (this.params.query.name) { searchOptions.name = this.params.query.name; }
+
+    placesSearch(searchOptions).bind(this)
+    .catch((err) => { this.respond(err); })
+    .then((places) => {
+      this.respond(places);
+    });
   }
 
   post() {
